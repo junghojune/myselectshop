7b21968941f5412f748dcc1a3c3bd0c7d822b735
hosu <hojune0904@gmail.com>
2024-08-11 18:52:03 +0900

    first commit

---
+++ src/main/java/com/sparta/myselectshop/exception/ProductNotFoundException.java
@@ -0,0 +1,1 @@
+package com.sparta.myselectshop.exception;public class ProductNotFoundException extends RuntimeException{    public ProductNotFoundException(String message) {        super(message);    }}
